529311ea4127ae378c66ff9fd67ad0a8d4e0dcd4
Pavel <ptupitsyn@apache.org>
2019-11-12 11:30:59 +0300

    Add test_all_cache_operations_with_affinity_aware_client_on_single_server - wip

--- tests/test_affinity_single_connection.py
+++ tests/test_affinity_single_connection.py
@@ -21,32 +21,49 @@ from tests.util import get_request_grid_idx
 def test_all_cache_operations_with_affinity_aware_client_on_single_server(client_affinity_aware_single_server):
     cache = client_affinity_aware_single_server.get_or_create_cache('test_cache_1')
     key = 1
+    key2 = 2
 
+    # Put/Get
     cache.put(key, key)
     assert cache.get(key) == key
 
-    res = cache.replace(key, key + 1)
+    # Replace
+    res = cache.replace(key, key2)
     assert res
+    assert cache.get(key) == key2
 
-    cache.clear_key(key)
-    assert cache.get(key) is None
+    # Clear
+    cache.put(key2, key2)
+    cache.clear_key(key2)
+    assert cache.get(key2) is None
 
-    cache.contains_key(key)
+    # ContainsKey
+    assert cache.contains_key(key)
+    assert not cache.contains_key(key2)
 
+    # GetAndPut
     cache.get_and_put(key, 3)
 
+    # GetAndPutIfAbsent
     cache.get_and_put_if_absent(key, 4)
 
+    # PutIfAbsent
     cache.put_if_absent(key, 5)
 
+    # GetAndRemove
     cache.get_and_remove(key)
 
+    # GetAndReplace
     cache.get_and_replace(key, 6)
 
+    # RemoveKey
     cache.remove_key(key)
 
+    # RemoveIfEquals
     cache.remove_if_equals(key, -1)
 
+    # Replace
     cache.replace(key, -1)
 
+    # ReplaceIfEquals
     cache.replace_if_equals(key, 10, -10)
