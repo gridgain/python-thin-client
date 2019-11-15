adf0ce05582df4c0a5cda9f18b122eeb65392688
Pavel <ptupitsyn@apache.org>
2019-11-15 21:55:00 +0300

    Add ticket number for failing tests on unsupported keys

--- tests/test_affinity_request_routing.py
+++ tests/test_affinity_request_routing.py
@@ -82,13 +82,13 @@ def test_cache_operation_on_primitive_key_routes_request_to_primary_node(
     assert get_request_grid_idx("ReplaceIfEquals") == grid_idx
 
 
-@pytest.mark.xfail(reason="Custom key objects are not supported yet")
+@pytest.mark.xfail(reason="GG-25823: Custom key objects are not supported yet")
 def test_cache_operation_on_complex_key_routes_request_to_primary_node():
     pass
 
 
 @pytest.mark.parametrize("key,grid_idx", [(1, 2), (2, 1), (3, 1), (4, 2), (5, 2), (6, 3)])
-@pytest.mark.xfail(strict=True,reason="Custom key objects are not supported yet")
+@pytest.mark.xfail(strict=True,reason="GG-25823: Custom key objects are not supported yet")
 def test_cache_operation_on_custom_affinity_key_routes_request_to_primary_node(
         request, client_affinity_aware, key, grid_idx):
     class AffinityTestType1(
