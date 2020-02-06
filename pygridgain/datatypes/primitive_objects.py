902b3b14a9a422125f47b91d8e7049dc41690b30
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-02-06 13:23:47 +0300

    Fixing BoolObject decoding

--- pygridgain/datatypes/primitive_objects.py
+++ pygridgain/datatypes/primitive_objects.py
@@ -207,7 +207,7 @@ class CharObject(DataObject):
 class BoolObject(DataObject):
     _type_name = NAME_BOOLEAN
     _type_id = TYPE_BOOLEAN
-    c_type = ctypes.c_bool
+    c_type = ctypes.c_byte
     type_code = TC_BOOL
     pythonic = bool
     default = False
@@ -215,3 +215,8 @@ class BoolObject(DataObject):
     @staticmethod
     def hashcode(value: bool, *args, **kwargs) -> int:
         return 1231 if value else 1237
+
+    @classmethod
+    def to_python(cls, ctype_object, *args, **kwargs):
+        return ctype_object != 0
+
