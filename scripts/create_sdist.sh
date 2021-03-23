e62fbb9a8fceafc9828330653657adda4443b220
Igor Sapego <isapego@apache.org>
2021-03-23 11:38:23 +0300

    GG-32909 IGNITE-14186 Implement C module to speedup hashcode (#28)
    (cherry picked from commit e5ca3fceb79a6c2e01c7480e56df162088fbedc0)
    
    Co-authored-by: Ivan Dashchinskiy <ivandasch@gmail.com>

---
+++ scripts/create_sdist.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright 2021 GridGain Systems, Inc. and Contributors.
+#
+# Licensed under the GridGain Community Edition License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e -u -x
+
+# Create source dist.
+for PYBIN in /opt/python/*/bin; do
+    if [[ $PYBIN =~ ^(.*)cp3[6789](.*)$ ]]; then
+        cd pygridgain
+        "${PYBIN}/python" setup.py sdist --formats=gztar,zip --dist-dir /dist
+        break;
+    fi
+done
+
+for archive in /dist/*; do
+    if [[ $archive =~ ^(.*)(tar\.gz|zip)$ ]]; then
+        chmod 666 "$archive"
+    fi
+done
+
+rm -rf /pygridgain/*.egg-info
+rm -rf /pygridgain/.eggs
--- setup.py
+++ setup.py
@@ -14,28 +14,45 @@
 # limitations under the License.
 #
 from collections import defaultdict
+from distutils.command.build_ext import build_ext
+from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
+
 import setuptools
 import sys
 
 
-PYTHON_REQUIRED = (3, 4)
-PYTHON_INSTALLED = sys.version_info[:2]
+cext = setuptools.Extension(
+    "pygridgain._cutils",
+    sources=[
+        "./cext/cutils.c"
+    ],
+    include_dirs=["./cext"]
+)
 
-if PYTHON_INSTALLED < PYTHON_REQUIRED:
-    sys.stderr.write('''
+if sys.platform == 'win32':
+    ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError, IOError, ValueError)
+else:
+    ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
 
-`pygridgain is not compatible with Python {}.{}!
-Use Python {}.{} or above.
+
+class BuildFailed(Exception):
+    pass
 
 
-'''.format(
-            PYTHON_INSTALLED[0],
-            PYTHON_INSTALLED[1],
-            PYTHON_REQUIRED[0],
-            PYTHON_REQUIRED[1],
-        )
-    )
-    sys.exit(1)
+class ve_build_ext(build_ext):
+    # This class allows C extension building to fail.
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except DistutilsPlatformError:
+            raise BuildFailed()
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except ext_errors:
+            raise BuildFailed()
 
 
 def is_a_requirement(line):
@@ -52,6 +69,7 @@ requirement_sections = [
     'tests',
     'docs',
 ]
+
 requirements = defaultdict(list)
 
 for section in requirement_sections:
@@ -68,38 +86,63 @@ for section in requirement_sections:
 with open('README.md', 'r', encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
-setuptools.setup(
-    name='pygridgain',
-    version='1.2.0',
-    python_requires='>={}.{}'.format(*PYTHON_REQUIRED),
-    author='Dmitry Melnichuk',
-    author_email='dmitry.melnichuk@nobitlost.com',
-    description='GridGain CE binary client Python API',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url=(
-        'https://github.com/gridgain/gridgain/tree/master'
-        '/modules/platforms/python'
-    ),
-    packages=setuptools.find_packages(),
-    install_requires=requirements['install'],
-    tests_require=requirements['tests'],
-    setup_requires=requirements['setup'],
-    extras_require={
-        'docs': requirements['docs'],
-    },
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3 :: Only',
-        'Intended Audience :: Developers',
-        'Topic :: Database :: Front-Ends',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: Free for non-commercial use',
-        'Operating System :: OS Independent',
-    ],
-)
+
+def run_setup(with_binary=True):
+    if with_binary:
+        kw = dict(
+            ext_modules=[cext],
+            cmdclass=dict(build_ext=ve_build_ext),
+        )
+    else:
+        kw = dict()
+
+    setuptools.setup(
+        name='pygridgain',
+        version='1.2.0',
+        python_requires='>=3.6',
+        author='GridGain Systems',
+        author_email='info@gridgain.com',
+        description='GridGain binary client Python API',
+        url='https://github.com/gridgain/python-thin-client',
+        packages=setuptools.find_packages(),
+        install_requires=requirements['install'],
+        tests_require=requirements['tests'],
+        setup_requires=requirements['setup'],
+        extras_require={
+            'docs': requirements['docs'],
+        },
+        classifiers=[
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3 :: Only',
+            'Intended Audience :: Developers',
+            'Topic :: Database :: Front-Ends',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: Free for non-commercial use',
+            'Operating System :: OS Independent',
+        ],
+        **kw
+    )
+
+
+try:
+    run_setup()
+except BuildFailed:
+    BUILD_EXT_WARNING = ("WARNING: The C extension could not be compiled, "
+                         "speedups are not enabled.")
+    print('*' * 75)
+    print(BUILD_EXT_WARNING)
+    print("Failure information, if any, is above.")
+    print("I'm retrying the build without the C extension now.")
+    print('*' * 75)
+
+    run_setup(False)
+
+    print('*' * 75)
+    print(BUILD_EXT_WARNING)
+    print("Plain python installation succeeded.")
+    print('*' * 75)
