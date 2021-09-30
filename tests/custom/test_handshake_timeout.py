89e86d223f6567ad64b2a3710cec318e0326eb18
Igor Sapego <isapego@apache.org>
2021-09-30 12:03:00 +0200

    GG-33939: Backport-related fixes

--- tests/custom/test_handshake_timeout.py
+++ tests/custom/test_handshake_timeout.py
@@ -1,18 +1,18 @@
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# Copyright 2021 GridGain Systems, Inc. and Contributors.
+#
+# Licensed under the GridGain Community Edition License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 import asyncio
 import logging
 import socket
@@ -22,10 +22,10 @@ from concurrent.futures import ThreadPoolExecutor
 
 import pytest
 
-from pyignite import Client, AioClient
-from pyignite import monitoring
-from pyignite.exceptions import ReconnectError, ParameterError
-from pyignite.monitoring import HandshakeFailedEvent
+from pygridgain import Client, AioClient
+from pygridgain import monitoring
+from pygridgain.exceptions import ReconnectError, ParameterError
+from pygridgain.monitoring import HandshakeFailedEvent
 
 logger = logging.getLogger('fake_ignite')
 logger.setLevel(logging.DEBUG)
