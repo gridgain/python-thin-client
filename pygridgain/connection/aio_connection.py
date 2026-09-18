9da66a3cc748c61d416df98a7bb072b521a3270d
Igor Sapego <isapego@apache.org>
2026-09-18 20:37:24 +0200

    GG-51636 get_event_loop() -> get_running_loop()

--- pygridgain/connection/aio_connection.py
+++ pygridgain/connection/aio_connection.py
@@ -138,7 +138,8 @@ class AioConnection(BaseConnection):
         super().__init__(client, host, port, username, password, **ssl_params)
         self._pending_reqs = {}
         self._transport = None
-        self._loop = asyncio.get_event_loop()
+        # Connections are only created from AioClient._connect(), so a loop is running.
+        self._loop = asyncio.get_running_loop()
         self._closed = False
         self._transport_closed_fut = None
 
